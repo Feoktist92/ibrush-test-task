aac4c8f67dc125cb59fecda1bc1c05d87c9bb94e
Igor Feoktistov <feoktist92@gmail.com>
2023-10-19 22:40:35 +0800

    last commit

--- src/slider.js
+++ src/slider.js
@@ -25,15 +25,12 @@ $(document).ready(function () {
 
 let slideIndex = 1;
 showSlides(slideIndex);
-
 function plusSlides(n) {
     showSlides((slideIndex += n));
 }
-
 function currentSlide(n) {
     showSlides((slideIndex = n));
 }
-
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName('mySlides');
